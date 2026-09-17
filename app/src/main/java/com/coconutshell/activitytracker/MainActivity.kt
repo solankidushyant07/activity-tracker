fc0c502030575c092496730f35ea6883983aa0dc
Dushyant Solanki <solankidushyant07@gmail.com>
2026-09-17 19:24:57 +0530

    Update MainActivity.kt

--- app/src/main/java/com/coconutshell/activitytracker/MainActivity.kt
+++ app/src/main/java/com/coconutshell/activitytracker/MainActivity.kt
@@ -14,7 +14,6 @@ import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.item
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
